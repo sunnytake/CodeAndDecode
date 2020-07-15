5d062eb124be93c8a55b3f4b3234a2dac1c6fa53
sunnytake <w13043750826>
2020-07-15 10:03:44 +0800

    最小元素栈-优化

--- 左神/初级班/第三课/同时返回最小元素栈.py
+++ 左神/初级班/第三课/同时返回最小元素栈.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 
 class StackWithMin:
+    '''
+    没有对最小栈进行优化
+    '''
     stack = []
     min_stack = []
 
@@ -22,6 +25,33 @@ class StackWithMin:
         else:
             return None
 
+
+class StackWithMin:
+    '''
+    对最小栈进行优化，空间复杂度降低
+    '''
+    stack = []
+    min_stack = []
+
+    def push(self, num):
+        self.stack.append(num)
+        # 只有在num小于 等于 最小栈栈顶的元素的时，才进入最小栈
+        if not self.min_stack or self.min_stack[-1] >= num:
+            self.min_stack.append(num)
+
+    def pop(self):
+        val = self.stack.pop()
+        # 只有在要弹出的元素 等于 最小栈栈顶元素时，最小栈栈顶元素也同时弹出
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+        return val
+
+    def getMin(self):
+        if self.min_stack:
+            return self.min_stack[-1]
+        else:
+            return None
+
 if __name__ == '__main__':
     stack = StackWithMin()
     stack.push(5)
@@ -29,6 +59,12 @@ if __name__ == '__main__':
     stack.push(3)
     stack.push(7)
     stack.push(1)
-    print(stack.getMin())
-    stack.pop()
-    print(stack.getMin())
+    print(stack.getMin()) # 1
+    stack.pop() # 弹出1
+    print(stack.getMin()) # 3
+    stack.pop() # 弹出7
+    print(stack.getMin()) # 3
+    stack.pop() # 弹出3
+    print(stack.getMin()) # 5
+    stack.pop() # 弹出10
+    print(stack.getMin()) # 5
